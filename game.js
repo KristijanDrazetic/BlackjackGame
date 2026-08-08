3d30d7ab97f2d2914e21620a7186d92a1e15416c
KristijanDrazetic <kristijan118@gmail.com>
2026-08-08 13:51:37 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,629 @@
+let cards = [{name: "2 of Clubs", value: 2, img: "Cards/Clubs/2 of Clubs.jpg"},
+    {name: "3 of Clubs", value: 3, img: "Cards/Clubs/3 of Clubs.jpg"},
+    {name: "4 of Clubs", value: 4, img: "Cards/Clubs/4 of Clubs.jpg"},
+    {name: "5 of Clubs", value: 5, img: "Cards/Clubs/5 of Clubs.jpg"},
+    {name: "6 of Clubs", value: 6, img: "Cards/Clubs/6 of Clubs.jpg"},
+    {name: "7 of Clubs", value: 7, img: "Cards/Clubs/7 of Clubs.jpg"},
+    {name: "8 of Clubs", value: 8, img: "Cards/Clubs/8 of Clubs.jpg"},
+    {name: "9 of Clubs", value: 9, img: "Cards/Clubs/9 of Clubs.jpg"},
+    {name: "10 of Clubs", value: 10, img: "Cards/Clubs/10 of Clubs.jpg"},
+    {name: "Ace of Clubs", value: 11, img: "Cards/Clubs/Ace of Clubs.jpg"},
+    {name: "Jack of Clubs", value: 10, img: "Cards/Clubs/Jack of Clubs.jpg"},
+    {name: "King of Clubs", value: 10, img: "Cards/Clubs/King of Clubs.jpg"},
+    {name: "Queen of Clubs", value: 10, img: "Cards/Clubs/Queen of Clubs.jpg"},
+
+    {name: "2 of Diamonds", value: 2, img: "Cards/Diamonds/2 of Diamonds.jpg"},
+    {name: "3 of Diamonds", value: 3, img: "Cards/Diamonds/3 of Diamonds.jpg"},
+    {name: "4 of Diamonds", value: 4, img: "Cards/Diamonds/4 of Diamonds.jpg"},
+    {name: "5 of Diamonds", value: 5, img: "Cards/Diamonds/5 of Diamonds.jpg"},
+    {name: "6 of Diamonds", value: 6, img: "Cards/Diamonds/6 of Diamonds.jpg"},
+    {name: "7 of Diamonds", value: 7, img: "Cards/Diamonds/7 of Diamonds.jpg"},
+    {name: "8 of Diamonds", value: 8, img: "Cards/Diamonds/8 of Diamonds.jpg"},
+    {name: "9 of Diamonds", value: 9, img: "Cards/Diamonds/9 of Diamonds.jpg"},
+    {name: "10 of Diamonds", value: 10, img: "Cards/Diamonds/10 of Diamonds.jpg"},
+    {name: "Ace of Diamonds", value: 11, img: "Cards/Diamonds/Ace of Diamonds.jpg"},
+    {name: "Jack of Diamonds", value: 10, img: "Cards/Diamonds/Jack of Diamonds.jpg"},
+    {name: "King of Diamonds", value: 10, img: "Cards/Diamonds/King of Diamonds.jpg"},
+    {name: "Queen of Diamonds", value: 10, img: "Cards/Diamonds/Queen of Diamonds.jpg"},
+
+    {name: "2 of Hearts", value: 2, img: "Cards/Hearts/2 of Hearts.jpg"},
+    {name: "3 of Hearts", value: 3, img: "Cards/Hearts/3 of Hearts.jpg"},
+    {name: "4 of Hearts", value: 4, img: "Cards/Hearts/4 of Hearts.jpg"},
+    {name: "5 of Hearts", value: 5, img: "Cards/Hearts/5 of Hearts.jpg"},
+    {name: "6 of Hearts", value: 6, img: "Cards/Hearts/6 of Hearts.jpg"},
+    {name: "7 of Hearts", value: 7, img: "Cards/Hearts/7 of Hearts.jpg"},
+    {name: "8 of Hearts", value: 8, img: "Cards/Hearts/8 of Hearts.jpg"},
+    {name: "9 of Hearts", value: 9, img: "Cards/Hearts/9 of Hearts.jpg"},
+    {name: "10 of Hearts", value: 10, img: "Cards/Hearts/10 of Hearts.jpg"},
+    {name: "Ace of Hearts", value: 11, img: "Cards/Hearts/Ace of Hearts.jpg"},
+    {name: "Jack of Hearts", value: 10, img: "Cards/Hearts/Jack of Hearts.jpg"},
+    {name: "King of Hearts", value: 10, img: "Cards/Hearts/King of Hearts.jpg"},
+    {name: "Queen of Hearts", value: 10, img: "Cards/Hearts/Queen of Hearts.jpg"},
+
+    {name: "2 of Spades", value: 2, img: "Cards/Spades/2 of Spades.jpg"},
+    {name: "3 of Spades", value: 3, img: "Cards/Spades/3 of Spades.jpg"},
+    {name: "4 of Spades", value: 4, img: "Cards/Spades/4 of Spades.jpg"},
+    {name: "5 of Spades", value: 5, img: "Cards/Spades/5 of Spades.jpg"},
+    {name: "6 of Spades", value: 6, img: "Cards/Spades/6 of Spades.jpg"},
+    {name: "7 of Spades", value: 7, img: "Cards/Spades/7 of Spades.jpg"},
+    {name: "8 of Spades", value: 8, img: "Cards/Spades/8 of Spades.jpg"},
+    {name: "9 of Spades", value: 9, img: "Cards/Spades/9 of Spades.jpg"},
+    {name: "10 of Spades", value: 10, img: "Cards/Spades/10 of Spades.jpg"},
+    {name: "Ace of Spades", value: 11, img: "Cards/Spades/Ace of Spades.jpg"},
+    {name: "Jack of Spades", value: 10, img: "Cards/Spades/Jack of Spades.jpg"},
+    {name: "King of Spades", value: 10, img: "Cards/Spades/King of Spades.jpg"},
+    {name: "Queen of Spades", value: 10, img: "Cards/Spades/Queen of Spades.jpg"},
+]
+
+function shuffle(){
+
+if(cards.length < 22){
+    
+    cards = [{name: "2 of Clubs", value: 2, img: "Cards/Clubs/2 of Clubs.jpg"},
+    {name: "3 of Clubs", value: 3, img: "Cards/Clubs/3 of Clubs.jpg"},
+    {name: "4 of Clubs", value: 4, img: "Cards/Clubs/4 of Clubs.jpg"},
+    {name: "5 of Clubs", value: 5, img: "Cards/Clubs/5 of Clubs.jpg"},
+    {name: "6 of Clubs", value: 6, img: "Cards/Clubs/6 of Clubs.jpg"},
+    {name: "7 of Clubs", value: 7, img: "Cards/Clubs/7 of Clubs.jpg"},
+    {name: "8 of Clubs", value: 8, img: "Cards/Clubs/8 of Clubs.jpg"},
+    {name: "9 of Clubs", value: 9, img: "Cards/Clubs/9 of Clubs.jpg"},
+    {name: "10 of Clubs", value: 10, img: "Cards/Clubs/10 of Clubs.jpg"},
+    {name: "Ace of Clubs", value: 11, img: "Cards/Clubs/Ace of Clubs.jpg"},
+    {name: "Jack of Clubs", value: 10, img: "Cards/Clubs/Jack of Clubs.jpg"},
+    {name: "King of Clubs", value: 10, img: "Cards/Clubs/King of Clubs.jpg"},
+    {name: "Queen of Clubs", value: 10, img: "Cards/Clubs/Queen of Clubs.jpg"},
+
+    {name: "2 of Diamonds", value: 2, img: "Cards/Diamonds/2 of Diamonds.jpg"},
+    {name: "3 of Diamonds", value: 3, img: "Cards/Diamonds/3 of Diamonds.jpg"},
+    {name: "4 of Diamonds", value: 4, img: "Cards/Diamonds/4 of Diamonds.jpg"},
+    {name: "5 of Diamonds", value: 5, img: "Cards/Diamonds/5 of Diamonds.jpg"},
+    {name: "6 of Diamonds", value: 6, img: "Cards/Diamonds/6 of Diamonds.jpg"},
+    {name: "7 of Diamonds", value: 7, img: "Cards/Diamonds/7 of Diamonds.jpg"},
+    {name: "8 of Diamonds", value: 8, img: "Cards/Diamonds/8 of Diamonds.jpg"},
+    {name: "9 of Diamonds", value: 9, img: "Cards/Diamonds/9 of Diamonds.jpg"},
+    {name: "10 of Diamonds", value: 10, img: "Cards/Diamonds/10 of Diamonds.jpg"},
+    {name: "Ace of Diamonds", value: 11, img: "Cards/Diamonds/Ace of Diamonds.jpg"},
+    {name: "Jack of Diamonds", value: 10, img: "Cards/Diamonds/Jack of Diamonds.jpg"},
+    {name: "King of Diamonds", value: 10, img: "Cards/Diamonds/King of Diamonds.jpg"},
+    {name: "Queen of Diamonds", value: 10, img: "Cards/Diamonds/Queen of Diamonds.jpg"},
+
+    {name: "2 of Hearts", value: 2, img: "Cards/Hearts/2 of Hearts.jpg"},
+    {name: "3 of Hearts", value: 3, img: "Cards/Hearts/3 of Hearts.jpg"},
+    {name: "4 of Hearts", value: 4, img: "Cards/Hearts/4 of Hearts.jpg"},
+    {name: "5 of Hearts", value: 5, img: "Cards/Hearts/5 of Hearts.jpg"},
+    {name: "6 of Hearts", value: 6, img: "Cards/Hearts/6 of Hearts.jpg"},
+    {name: "7 of Hearts", value: 7, img: "Cards/Hearts/7 of Hearts.jpg"},
+    {name: "8 of Hearts", value: 8, img: "Cards/Hearts/8 of Hearts.jpg"},
+    {name: "9 of Hearts", value: 9, img: "Cards/Hearts/9 of Hearts.jpg"},
+    {name: "10 of Hearts", value: 10, img: "Cards/Hearts/10 of Hearts.jpg"},
+    {name: "Ace of Hearts", value: 11, img: "Cards/Hearts/Ace of Hearts.jpg"},
+    {name: "Jack of Hearts", value: 10, img: "Cards/Hearts/Jack of Hearts.jpg"},
+    {name: "King of Hearts", value: 10, img: "Cards/Hearts/King of Hearts.jpg"},
+    {name: "Queen of Hearts", value: 10, img: "Cards/Hearts/Queen of Hearts.jpg"},
+
+    {name: "2 of Spades", value: 2, img: "Cards/Spades/2 of Spades.jpg"},
+    {name: "3 of Spades", value: 3, img: "Cards/Spades/3 of Spades.jpg"},
+    {name: "4 of Spades", value: 4, img: "Cards/Spades/4 of Spades.jpg"},
+    {name: "5 of Spades", value: 5, img: "Cards/Spades/5 of Spades.jpg"},
+    {name: "6 of Spades", value: 6, img: "Cards/Spades/6 of Spades.jpg"},
+    {name: "7 of Spades", value: 7, img: "Cards/Spades/7 of Spades.jpg"},
+    {name: "8 of Spades", value: 8, img: "Cards/Spades/8 of Spades.jpg"},
+    {name: "9 of Spades", value: 9, img: "Cards/Spades/9 of Spades.jpg"},
+    {name: "10 of Spades", value: 10, img: "Cards/Spades/10 of Spades.jpg"},
+    {name: "Ace of Spades", value: 11, img: "Cards/Spades/Ace of Spades.jpg"},
+    {name: "Jack of Spades", value: 10, img: "Cards/Spades/Jack of Spades.jpg"},
+    {name: "King of Spades", value: 10, img: "Cards/Spades/King of Spades.jpg"},
+    {name: "Queen of Spades", value: 10, img: "Cards/Spades/Queen of Spades.jpg"},
+]
+        }
+}
+
+let playerCountDisplay = document.getElementById("playerCount")
+let dealerCountDisplay = document.getElementById("dealerCount")
+let dealerContainer = document.getElementById("containerDealer")
+let playerContainer = document.getElementById("containerPlayer")
+let bet = 0
+let playerMoney = 1000
+let dealerCount = 0
+let playerCount = 0
+let dealerArray = []
+let playerArray = []
+let playerMoneyDisplay = document.getElementById("playerMoney")
+let betDisplay = document.getElementById("playerBet")
+let handEnd = false
+let btnSoundClick = new Audio("Sounds/btnSound.mp3");
+let chipSoundClick = new Audio("Sounds/chipSound.mp3");
+let cardSound = new Audio("Sounds/cardSound.mp3");
+let playerWinSound = new Audio("Sounds/playerWin.mp3");
+let dealerWinSound = new Audio("Sounds/dealerWin.mp3");
+let playerDrawSound = new Audio("Sounds/drawSound.mp3");
+let gameSound = new Audio("Sounds/gameMusic.m4a");
+let wrongSound = new Audio("Sounds/wrong.mp3");
+let betResetBtns = document.querySelectorAll(".resetBet")
+let betTwetny = document.getElementById("chipTwenty")
+let betFifty = document.getElementById("chipFifty")
+let betHundert = document.getElementById("chipOneHundred")
+let betAllIn = document.getElementById("allIn")
+let dealBtn = document.getElementById("dealBtn")
+let hitBtn = document.getElementById("hitBtn")
+let standBtn = document.getElementById("standBtn")
+let hiddenCard = document.getElementById("containerDealer")
+let cardDeckCount = document.getElementById("cardDeckCount")
+
+
+function cardCount(){
+cardDeckCount.innerHTML = cards.length
+}
+
+
+
+function playSounds(sound){
+  sound.play()
+}
+
+function getCard(){
+
+  for(let i = 0; i < 1; i++){
+
+    if(cards.length != -1){
+       
+      let randomCard =  Math.floor(Math.random()*cards.length)
+
+      return x = cards.splice(randomCard, 1);}
+    }
+}
+
+
+function checkAcePlayer(card){
+  
+  if(card[0].value === 11 && playerCount < 21 || card[0].value === 11 && playerCount === 21){
+      
+    playerArray.push(card[0].value)
+  }
+
+    if(playerCount > 21 && card[0].value === 11){
+
+      playerCountDisplay.innerText = `Player: ${playerCount -= 10}`
+
+    }
+
+    if(playerArray.includes(11) && playerCount > 21){
+
+      playerCount -=10  
+      playerArray = []
+      playerCountDisplay.innerText = `Player: ${playerCount}`
+
+    }  
+
+}
+
+function checkAceDealer(card){
+
+  if(card[0].value === 11 && dealerCount <= 21 ){
+
+    dealerArray.push(card[0].value)
+
+  }
+
+  if(dealerCount > 21 && card[0].value === 11){
+
+    dealerCountDisplay.innerText = `Dealer: ${dealerCount -= 10}`
+
+  }
+  
+  if(dealerCount === 12 && dealerArray.length === 1){
+
+    dealerCountDisplay.innerText = `Dealer: ${11}`
+
+  }
+
+  if(dealerArray.includes(11) && dealerCount > 21){
+
+    dealerCount -=10
+    dealerArray = []
+    dealerCountDisplay.innerText = `Dealer: ${dealerCount}`
+
+  }  
+
+}
+
+function getDealerCard(){
+
+  let card = getCard()
+  let firstDealerCard = document.createElement("div")
+
+  firstDealerCard.innerHTML = `<div class="card">
+              <img
+                class="cards"
+                src="${card[0].img}"
+                alt=""
+              />
+            </div>`
+   
+  dealerContainer.append(firstDealerCard)
+  dealerCountDisplay.innerText = `Dealer: ${dealerCount += card[0].value}`
+
+  checkAceDealer(card)
+    
+}
+
+function getDealerCardSecondCard(){
+
+  let card = getCard()
+
+  let firstDealerCard = document.createElement("div")
+  firstDealerCard.innerHTML = `<div class="card">
+              <img
+                class="cards"
+                src="${card[0].img}"
+                alt=""
+              />
+            </div>`
+  dealerContainer.append(firstDealerCard)
+
+  dealerCount  += card[0].value
+  
+  checkAceDealer(card)
+
+}
+
+function getPlayerCard(){
+
+  let card = getCard()
+
+  let firstPlayerCard = document.createElement("div")
+  firstPlayerCard.innerHTML = `<div class="card">
+              <img
+                class="cards" 
+                src="${card[0].img}"
+                alt=""
+              />
+            </div>`
+  playerContainer.append(firstPlayerCard)
+ 
+  playerCountDisplay.innerText = `Player: ${playerCount += card[0].value}`
+
+  checkAcePlayer(card)
+   
+}
+
+function deal(){
+
+
+
+
+ if(bet === 0 || playerCount > 1){
+    
+    playSounds(wrongSound)
+    return
+
+  }
+  
+  playSounds(btnSoundClick)
+  
+
+ if(playerCount === 0 && dealerCount === 0){ 
+  
+    setTimeout(() =>{
+    getDealerCard()
+    cardCount()
+
+    getDealerCardSecondCard()
+    cardCount()
+
+    getPlayerCard()
+    cardCount()
+    playSounds(cardSound)  
+
+    }, 100)
+  }
+ 
+
+  
+}
+
+function hit(){
+
+  if(handEnd === true){
+
+    playSounds(wrongSound)
+    return
+
+  }
+  
+  if(dealerCount === 0){
+
+    playSounds(wrongSound)
+
+  }
+  
+  if(dealerCount > 0){
+  
+    playSounds(cardSound)
+    playSounds(btnSoundClick)
+
+    setTimeout(() => {
+ 
+    if(playerCount > 0 && playerCount < 21){
+
+      getPlayerCard()
+      cardCount()
+    }
+  
+    if(playerCount > 21 || playerCount === 21){
+    
+      stand()
+
+    }
+      }, 700);
+
+  }
+
+}
+
+function displayGameLost(){
+ 
+  playerContainer.innerHTML = `
+  <article class="gameEndContainer" >
+              <p>NO MONEY LEFT!</p>
+              <br />
+              
+              <button id="playAgainBtn">
+                <p class="playingBtns">PLAY AGAIN!</p></button
+              ><br /><button id="leaveGameBtn">
+                <p class="playingBtns">LEAVE GAME!</p>
+              </button>
+            </article>
+  `
+
+    function setPlayerMoney(){
+      playerMoney = 1000
+      
+    }
+
+
+  let playAgainBtn = document.getElementById("playAgainBtn")
+  playAgainBtn.addEventListener("click", setPlayerMoney)
+  playAgainBtn.addEventListener("click", resetBoard)
+
+
+  function leaveGame(){
+    window.open('','_self').close()
+
+  }
+
+  let leaveGameBtn = document.getElementById("leaveGameBtn")
+  leaveGameBtn.addEventListener("click", leaveGame)
+
+}
+
+function resetBoard(){
+
+  playSounds(btnSoundClick)
+  dealerCountDisplay.innerText = ""
+  playerCountDisplay.innerText = ""
+  betDisplay.innerHTML = ``
+  bet = 0
+  dealerCount = 0
+  playerCount = 0
+  dealerArray = []
+  playerArray = []
+  dealerSecondCard = []
+  playerContainer.innerHTML = ""
+  dealerContainer.innerHTML = ""
+  handEnd = false
+  playerMoneyDisplay.innerHTML = `Bank: $${playerMoney}`
+  document.getElementById("displayBetChipTwenty").style.zIndex = -1
+  document.getElementById("displayBetChipFifty").style.zIndex = -1
+  document.getElementById("displayBetChipHundert").style.zIndex = -1
+  cardCount()
+}
+
+function betReset(){
+  
+  if(playerCount > 0){
+    playSounds(wrongSound)
+    return
+  }
+  
+  playSounds(chipSoundClick)
+  playerMoney = playerMoney + bet
+  bet = 0
+  betDisplay.innerHTML = ``
+  document.getElementById("displayBetChipTwenty").style.zIndex = -1
+  document.getElementById("displayBetChipFifty").style.zIndex = -1
+  document.getElementById("displayBetChipHundert").style.zIndex = -1
+  playerMoneyDisplay.innerHTML = `Bank: $${playerMoney}`
+
+}
+
+betResetBtns.forEach(btn => {
+    btn.addEventListener("click", ()=> {
+      betReset()
+    
+    })
+})
+
+function displayGameResult(){
+
+  let displayGameResultContainer = document.getElementById("playerCount")
+  displayGameResultContainer.innerHTML  = ` Player: ${playerCount}
+
+  <br/>
+  <br/>
+  <div>
+    
+  <div class ="gameResultContainer"><p></p><p>${gameResult()}</p> </br> </br> <button id ="resetBtn">OK</button></div> 
+
+  </div>`
+  
+  let resetBtn = document.getElementById("resetBtn")
+  resetBtn.addEventListener("click",resetBoard)
+  if (playerMoney === 0){
+    resetBtn.addEventListener("click",displayGameLost)
+  }
+
+}
+
+function gameResult(){
+
+  shuffle()
+  
+
+  switch(true){
+  case dealerCount === playerCount && playerCount !=0:
+  playerMoney += bet
+  bet= 0
+  playSounds(playerDrawSound)
+  return "Draw!"
+  break;
+  case dealerCount > 21 && playerCount < 22:
+  playerMoney += bet * 2
+  bet= 0
+  playSounds(playerWinSound)
+  return "Player Win!"
+  break;
+  case dealerCount < 21 && playerCount > 21:
+  playSounds(dealerWinSound)
+  return "Dealer Win!"
+  break;
+  case dealerCount < playerCount && playerCount  < 22:
+  playerMoney +=bet * 2
+  bet = 0
+  playSounds(playerWinSound)
+  return "Player Win!"
+  break;
+  case dealerCount > playerCount && dealerCount < 22:
+  playSounds(dealerWinSound)
+  return "Dealer Win!"
+  break
+  case dealerCount === 21 && playerCount > 21:
+  playSounds(dealerWinSound)
+  return "Dealer Win!"
+  break
+  case dealerCount > 21 && playerCount > 21:
+  playSounds(dealerWinSound)
+  return "Dealer Win!"
+  break
+}
+
+}
+
+function stand(){
+
+  if(dealerCount === 0 || handEnd === true){
+   playSounds(wrongSound)
+    return
+  }
+
+  playSounds(btnSoundClick)
+
+  handEnd = true
+  hiddenCard.children[1].firstChild.children[0].style.content =  "unset"
+
+  setTimeout(() => {
+    if(dealerCount !== 0 && playerCount !== 0 ){
+      for (let step = 0; dealerCount < 17; step++) {
+
+        playSounds(cardSound)
+        getDealerCard()
+        cardCount()
+      }
+
+      gameResult()
+      displayGameResult()
+  
+    }  
+
+  }, 2000);
+
+  dealerCountDisplay.innerText = `Dealer: ${dealerCount}`
+
+  if(dealerCount > 1){
+    return
+  }
+
+}
+
+function betChip(chipValue){
+
+  if(playerCount > 0){
+    playSounds(wrongSound)
+    return
+  }
+
+  if(playerMoney < chipValue){
+    playSounds(wrongSound)
+    return
+  }
+
+ playSounds(chipSoundClick)
+ displayBetChips(chipValue)
+
+  bet += chipValue
+  playerMoney -= chipValue
+  betDisplay.innerHTML = `Bet: $${bet}`
+  playerMoneyDisplay.innerHTML =  `Bank: $${playerMoney}`
+
+}
+
+function allIn(){
+ 
+  if(playerCount > 0 || playerMoney === 0){
+    playSounds(wrongSound)
+    return
+  }
+
+  playSounds(btnSoundClick)
+  bet += playerMoney
+
+  if(playerMoney != 0){
+    betDisplay.innerHTML = `Bet: $${bet }`
+    document.getElementById("displayBetChipTwenty").style.zIndex = 1
+    document.getElementById("displayBetChipFifty").style.zIndex = 1
+    document.getElementById("displayBetChipHundert").style.zIndex = 1
+  }
+  
+  playerMoneyDisplay.innerHTML =  `Bank: $${playerMoney - playerMoney}`
+  playerMoney -= playerMoney
+
+}
+
+function displayBetChips(betChipValue){
+
+  if(betChipValue === 20){
+    document.getElementById("displayBetChipTwenty").style.zIndex = 1
+  }else if(betChipValue === 50){
+    document.getElementById("displayBetChipFifty").style.zIndex = 1
+  }else if(betChipValue === 100){
+    document.getElementById("displayBetChipHundert").style.zIndex = 1
+  }
+ 
+}
+
+const element = document.querySelector('div :first-child');
+
+element.addEventListener('click', function() {
+  // 'toggle' dodaje klasu ako ne postoji, ili je miče ako već postoji
+  this.classList.toggle('active');
+});
+
+betTwetny.addEventListener("click",betChip.bind(null, 20))
+betFifty.addEventListener("click",betChip.bind(null, 50))
+betHundert.addEventListener("click",betChip.bind(null, 100))
+betAllIn.addEventListener("click",allIn)
+dealBtn.addEventListener("click",deal)
+hitBtn.addEventListener("click",hit)
+standBtn.addEventListener("click",stand)
+
+
+
+
+
+
+
+
+
+
+
+
